a35281b33d911ab72cea9dea65aef85e8c919351
rilith <bludoftenchiefs@protonmail.com>
2021-05-02 10:46:50 -0500

    commit tree 21c3e7db8f5dd953b320657e8d029e5a95b386dd

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
 
-// Write your code here
+console.log(multiply);
+
+let random = 0;
+random++;
+console.log(random);
+
+let num3 = 9000;
+let num4 = 13;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = (17,18,19,20);
+Math.max(max);
